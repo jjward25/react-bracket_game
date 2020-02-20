401e30f644cab93d94e7592d6c249c050c289041
Joe <jjward25@gmail.com>
2020-02-20 13:38:10 -0500

    Bracket Version 5: Re-Sized Menu

--- src/modules/menu/Menu.js
+++ src/modules/menu/Menu.js
@@ -5,8 +5,11 @@ import './Menu.css';
 function Menu() {
   return (
     <div className="Menu">
+      <p className="Menu-text" id="mt1">
+        The Bracket Game
+        </p>
       <img src={logo} className="Menu-logo" alt="logo" />
-      <p>
+      <p className="Menu-text" id="mt2">
         Select Game Type
         </p>
       <button className="Menu-button" id="b1">
