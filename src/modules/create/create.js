37b62a0009bd1ee4e96e1fbf812b367acb7288a7
Joe <jjward25@gmail.com>
2020-02-20 21:27:19 -0500

    Bracket Version 8: Visible Form ReSave

--- src/modules/create/create.js
+++ src/modules/create/create.js
@@ -27,40 +27,40 @@ class Create extends React.Component {
                 <p> Enter Your Contestants </p>
 
                 <div className="form-label">
-                    <input className="form-input" id="input1" type="text" />
+                    <input className="form-input" id="input1" type="text" value={this.state.input1} onChange={this.handleInput1Change} />
                 </div>
 
                 <div className="form-label">
-                    <input className="form-input" id="input2" type="text" />
+                    <input className="form-input" id="input2" type="text" value={this.state.input2} onChange={this.handleInput2Change} />
                 </div>
 
                 <div className="form-label">
-                    <input className="form-input" id="input3" type="text" />
+                    <input className="form-input" id="input3" type="text" value={this.state.input3} onChange={this.handleInput3Change} />
                 </div>
 
                 <div className="form-label">
-                    <input className="form-input" id="input4" type="text" />
+                    <input className="form-input" id="input4" type="text" value={this.state.input4} onChange={this.handleInput4Change} />
                 </div>
 
                 <div className="form-label">
-                    <input className="form-input" id="input5" type="text" />
+                    <input className="form-input" id="input5" type="text" value={this.state.input5} onChange={this.handleInput5Change} />
                 </div>
 
                 <div className="form-label">
-                    <input className="form-input" id="input6" type="text" />
+                    <input className="form-input" id="input6" type="text" value={this.state.input6} onChange={this.handleInput6Change} />
                 </div>
 
                 <div className="form-label">
-                    <input className="form-input" id="input7" type="text" />
+                    <input className="form-input" id="input7" type="text" value={this.state.input7} onChange={this.handleInput7Change} />
                 </div>
 
                 <div className="form-label">
-                    <input className="form-input" id="input8" type="text" />
+                    <input className="form-input" id="input8" type="text" value={this.state.input8} onChange={this.handleInput8Change} />
                 </div>
 
                 <p> Enter Your Contest Category </p>
                 <div className="form-label">
-                    <input className="form-input" id="input9" type="text" />
+                    <input className="form-input" id="input9" type="text" value={this.state.input9} onChange={this.handleInput9Change} />
                 </div>
 
                 <input className="form-submit" type="submit" value="Submit" />
