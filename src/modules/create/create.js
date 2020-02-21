13140b4fd3ed44820453c13dd32eca05687a5517
Joe <jjward25@gmail.com>
2020-02-20 21:09:04 -0500

    Bracket Version 8: Visible Form

--- src/modules/create/create.js
+++ src/modules/create/create.js
@@ -1,17 +1,75 @@
 import React from 'react';
 import './create.css';
 
-function Create() {
-    return (
-        <div className="create-form">
-            <p>
-                Enter Your Contestants
-        </p>
 
+class Create extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: 'Contestants' };
 
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-        </div>
-    );
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+
+            <form className="create-form" onSubmit={this.handleSubmit}>
+                <p> Enter Your Contestants </p>
+
+                <div className="form-label">
+                    <input className="form-input" id="input1" type="text" />
+                </div>
+
+                <div className="form-label">
+                    <input className="form-input" id="input2" type="text" />
+                </div>
+
+                <div className="form-label">
+                    <input className="form-input" id="input3" type="text" />
+                </div>
+
+                <div className="form-label">
+                    <input className="form-input" id="input4" type="text" />
+                </div>
+
+                <div className="form-label">
+                    <input className="form-input" id="input5" type="text" />
+                </div>
+
+                <div className="form-label">
+                    <input className="form-input" id="input6" type="text" />
+                </div>
+
+                <div className="form-label">
+                    <input className="form-input" id="input7" type="text" />
+                </div>
+
+                <div className="form-label">
+                    <input className="form-input" id="input8" type="text" />
+                </div>
+
+                <p> Enter Your Contest Category </p>
+                <div className="form-label">
+                    <input className="form-input" id="input9" type="text" />
+                </div>
+
+                <input className="form-submit" type="submit" value="Submit" />
+
+            </form>
+
+        );
+    }
 }
+
 
 export default Create;
