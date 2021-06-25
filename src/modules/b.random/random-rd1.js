ab7a8c227eb6a7692361de444d437ecb27d5c839
jjward25 <45015765+jjward25@users.noreply.github.com>
2021-06-25 13:22:41 -0400

    random pre status

--- src/modules/b.random/random-rd1.js
+++ src/modules/b.random/random-rd1.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import games from '../../bracket_list.json'
+import './random.css'
 
 
-const randomGame = games.main_game_list[Math.floor(Math.random() * games.main_game_list.length)]
-const seeds = Object.values(randomGame);
+const randomGame = games.main_game_list[Math.floor(Math.random() * games.main_game_list.length)];
+let noms = Object.values(randomGame);
+noms = noms.filter(nom => nom != randomGame.category)
+const seeds = ['1','8','4','5','2','7','3','6']
+
 console.log(randomGame)
-console.log(seeds)
+console.log(noms)
 
 function RandomRd1() {  
 return (
@@ -17,22 +21,18 @@ return (
             Category: {randomGame.category}
         </div>
 
-        <div id="mt2">
-            Nominees
-        </div>
-
         <div>
             <ul className="random-rd1">
 
-                {seeds.map((seed, index) => {
+                {noms.map((nom, index) => {
                     
-                    return  <li className="nom-rd1" key={index} style= {{ textDecoration: 'none' }} >
-                                <p>{seed}</p>
+                    return  <li className="nom-rd1" key={index}>
+                                <p id="random-rd1-seed">{seeds[index]}</p>
+                                <p>{nom}</p>
                             </li>;
                 })}
             </ul>
         </div>
-
 
         </div>
     </div>    
